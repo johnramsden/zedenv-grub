c82399542af34c8d0223105c3917064805849ca7
John Ramsden <johnramsden@riseup.net>
2018-06-16 00:15:00 -0700

    Add function to check file is valid
    Equivalent to grub_file_is_not_garbage() function

--- grub.d/10_zfs_linux.py
+++ grub.d/10_zfs_linux.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3.6
 
 import os
+import platform
 import subprocess
+
+from typing import List
+
+import pyzfscmds.utility
+import pyzfscmds.system.agnostic
+import zedenv.lib.be
 
 
 def source(file: str):
@@ -64,3 +71,81 @@ class Generator:
         if "GRUB_DISABLE_LINUX_PARTUUID" in os.environ:
             if os.environ['GRUB_DISABLE_LINUX_PARTUUID'] == ("false" or "False" or "0"):
                 self.grub_disable_linux_partuuid = False
+
+        self.root_dataset = zedenv.lib.be.root()
+        # in GRUB terms, bootfs is everything after pool
+        self.bootfs = "/" + self.root_dataset.split("/", 1)[1]
+        self.rpool = pyzfscmds.system.agnostic.mountpoint_dataset("/").split("/")[0]
+        self.linux_root_device=f"ZFS={self.rpool}{self.bootfs}"
+
+        self.machine = platform.machine()
+
+        self.invalid_filenames = ["readme"]  # Normalized to lowercase
+        self.invalid_extensions = [".dpkg", ".rpmsave", ".rpmnew", ".pacsave", ".pacnew"]
+
+    def file_valid(self, file_path):
+        """
+        Run equivalent checks to grub_file_is_not_garbage() from grub-mkconfig_lib
+        Check file is valid and not one of:
+        *.dpkg - debian dpkg
+        *.rpmsave | *.rpmnew
+        README* | */README* - documentation
+        """
+        if not os.path.isfile(file_path):
+            return False
+
+        file = os.path.basename(file_path)
+        _, ext = os.path.splitext(file)
+
+        if ext in self.invalid_extensions:
+            return False
+
+        if next((True for f in self.invalid_filenames if f.lower() in file.lower()), False):
+            return False
+
+        return True
+
+    def get_boot_list(self):
+        """
+        Check if grub list item shows up
+
+        machine=`uname -m`
+        case "x$machine" in
+            xi?86 | xx86_64)
+            list=
+            for i in /boot/vmlinuz-* /vmlinuz-* /boot/kernel-* ; do
+                if grub_file_is_not_garbage "$i" ; then list="$list $i" ; fi
+            done ;;
+            *)
+            list=
+            for i in /boot/vmlinuz-* /boot/vmlinux-* /vmlinuz-* /vmlinux-* /boot/kernel-* ; do
+                          if grub_file_is_not_garbage "$i" ; then list="$list $i" ; fi
+            done ;;
+        esac
+
+        case "$machine" in
+            i?86) GENKERNEL_ARCH="x86" ;;
+            mips|mips64) GENKERNEL_ARCH="mips" ;;
+            mipsel|mips64el) GENKERNEL_ARCH="mipsel" ;;
+            arm*) GENKERNEL_ARCH="arm" ;;
+            *) GENKERNEL_ARCH="$machine" ;;
+        esac
+        """
+        pass
+
+    def grub_command(self, command: str, call_args: List[str] = None):
+
+        cmd_call = [command]
+        if call_args:
+            cmd_call.extend(call_args)
+
+        try:
+            cmd_output = subprocess.check_output(
+                cmd_call, universal_newlines=True, stderr=subprocess.PIPE)
+        except subprocess.CalledProcessError as e:
+            raise RuntimeError(f"Failed to run {command}.\n{e}\n.")
+
+        return cmd_output.splitlines()
+
+
+grub = Generator()
